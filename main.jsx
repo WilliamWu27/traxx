38bbcbf74549a1a73a29f27d322a29db784ed87f
WilliamWu27 <63175004+WilliamWu27@users.noreply.github.com>
2026-02-24 14:04:37 -0500

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,16 +1,15 @@
-// src/firebase.js
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// TODO: Replace with your Firebase config from Firebase Console
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA7tZeM76MJYZlh-eX2PKrcj77VbR5XsPE",
+  authDomain: "trytraxapp.firebaseapp.com",
+  projectId: "trytraxapp",
+  storageBucket: "trytraxapp.firebasestorage.app",
+  messagingSenderId: "312292654300",
+  appId: "1:312292654300:web:c0a04772874ea4be67c819",
+  measurementId: "G-6BDD662RXP"
 };
 
 // Initialize Firebase
